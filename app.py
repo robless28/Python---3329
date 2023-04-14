c9b66e69fb9dee534eb180cf0c214527a8f4331c
robless28 <maria.gonzalezrobles01@utrgv.edu>
2023-04-13 20:37:53 -0500

    changes done rn

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('homepage.html')
-# flask run    # python app.py
-
-@app.route("/rockets")
-def anotherPage():
-    return "<p> This is another page </p> <a href='google.com'> Link to another site </a>"
 
 # Function to read details for page 
 def readDetails(filename):
     with open(filename, 'r') as f:
-        return [line for line in f]
+        return [line for line in f] 
     
 def writeToFile(filename, message):
     with open(filename, 'a') as f:
         f.write(message)
+
+
+@app.route('/')
+def homePage():
+    name = "My Page"
+    details = readDetails('static/details.txt')
+    return render_template('homepage.html', name = name, aboutMe = details)
+# flask run    # python app.py
+
 
 ################################
 
@@ -27,16 +28,16 @@ def writeToFile(filename, message):
 def greet(name):
     return f'<p>Hello, {name}!</p>'
 
-@app.route('/form', methods=['GET', 'POST'])
+@app.route('/slay', methods=['GET', 'POST'])
 def formDemo():
     name = None
     if request.method == 'POST':
-        name = request.form['name']
-        # if request.form['message']:
-            # writeToFile('/static/comments.txt', request.form['messgae'])
+        # name = request.form['name']
+        if request.form['message']:
+            writeToFile('/static/comments.txt', request.form['messgae'])
 
     return render_template('form.html', name=name)
 
 ## When running this file directtly...
-if __name__ == "__main__":
-    app.run(debug=True, port=2000)
+if __name__ == '__main__':
+    app.run(debug=True)
